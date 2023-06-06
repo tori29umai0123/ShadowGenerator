d4af067a57b88ca0a85e9013ce25f85f7917d136
tori29umai <72191117+tori29umai0123@users.noreply.github.com>
2023-06-07 08:15:22 +0900

    Update imread.py

--- imread.py
+++ imread.py
@@ -18,6 +18,13 @@ def resize_image(image,max_size):
     # 画像のサイズを取得
     height, width, _ = image.shape
 
+    if width % 8 == 0 and height % 8 == 0:
+        return
+
+    # 画像のサイズを8の倍数に丸める
+    new_width = round(width / 8) * 8
+    new_height = round(height / 8) * 8
+
     # アスペクト比を保ちつつ長辺がmax_size以内になるようにリサイズ
     aspect_ratio = width / height
     if aspect_ratio >= 1:
@@ -28,12 +35,6 @@ def resize_image(image,max_size):
         if height > width:
             new_height = max_size
             new_width = round(new_height * aspect_ratio)
-
-    if new_width % 2 != 0:
-        new_width = new_width + 1
-
-    if new_height % 2 != 0:
-        new_height = new_height + 1
 
     resized_image = cv2.resize(image, (new_width, new_height))
     return resized_image
